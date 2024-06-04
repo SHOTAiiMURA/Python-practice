2c62c7f6c71b29052679e9a481ba54abdde7afec
SHOTAiiMURA <hn.j.shota027503@gamil.com>
2024-06-04 19:33:39 +0900

    initial commit day 3

---
+++ Python practice day3.py
@@ -0,0 +1,20 @@
+# .format(): Use string formatting with .format() to create a formatted string1
+# .join(): Combine elements from a list into a single string using .join()1
+# .split(): Split a string into a list of substrings based on a delimiter1
+# .strip(): Remove leading and trailing whitespace from a string1
+# dir(): Explore the attributes and methods of an object1
+# Nested Data: Work with nested lists or dictionaries1
+# Conditionals (if): Write conditional statements to control program flow1
+# For Loop: Iterate over a sequence using a for loop1
+# While Loop: Use a while loop to repeat a block of code1
+# Break & Continue: Control loop execution with break and continue1
+# Lambda Functions: Create anonymous functions using lambda1
+# zip(): Combine multiple iterables into tuples1
+# map(): Apply a function to each element in a list1
+# filter(): Filter elements based on a condition1
+# sorted(): Sort a list of elements1
+# List Comprehension: Create concise lists using list comprehensions1
+# Dict Comprehension: Construct dictionaries using comprehensions1
+# help(): Access documentation for Python objects1
+# Debugging: Learn common debugging techniques1
+# pass: Use the pass statement as a placeholder1
